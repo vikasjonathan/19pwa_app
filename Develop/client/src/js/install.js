717c8afddc75077fe9b7bc21cbe56896596f3ad1
Vikas David Jonathan <vikas.jonathan@gmail.com>
2024-03-07 13:29:11 -0600

    cleared prompt after installation

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -30,4 +30,7 @@ butInstall.addEventListener('click', async () => {
 
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => { });
+window.addEventListener('appinstalled', (event) => {
+    //clear 
+    window.deferredPrompt = null;
+});
